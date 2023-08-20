8291d733ed292b5249398597d2a89831736813cf
jorgezeza <jorgezeza2010@gmail.com>
2023-08-20 16:16:02 -0300

    style: rearrange styles in global folder close #5

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,9 +2,8 @@ import Image from "next/image"
 import Link from "next/link"
 
 const NavBar = ({ openMenu }:any) => {
-  console.log('Open Menu:', openMenu)
   return (
-    <div className={`bg-black w-1/2 h-screen text-white md:w-1/3 lg:w-1/6 relative -left-[999px] transition-all ease-in-out duration-1000 ${openMenu ? 'menu__transition' : ''}`}>
+    <div className={`container__nav-menu ${openMenu ? 'menu__transition' : ''}`}>
       <div className="relative flex flex-col text-5xl font-secondary">
         <div className="absolute -top-5 right-0" >
           <Image
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image'
-import Header from './components/Header'
 
 export default function Home () {
   return (
     <>
-      <Header />
+      <h1>oooooooooooooi</h1>
       
     </>
   )
