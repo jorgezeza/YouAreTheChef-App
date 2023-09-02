ea37f9960af5a4a07e32cbfa9c1db782906104e9
jorgezeza <jorgezeza2010@gmail.com>
2023-09-02 16:38:26 -0300

    feat: close NavBar menu with click out close #21

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   const [openMenu, setOpenMenu] = useState(false)
   return (
     <>
-      <header className="flex items-center w-full h-14 bg-primaryColor text-white px-6 " >
+      <header className="flex items-center w-full h-14 bg-primaryColor text-white px-6" >
         <div className="flex items-center gap-2 text-2xl " >
           <button className={`relative w-8 h-8`} onClick={() => setOpenMenu(!openMenu)}>
             <Image
@@ -19,7 +19,14 @@ const Header = () => {
           <span>MINHA PIZZA</span>
         </div>
       </header>
+
       <NavBar openMenu={openMenu} />
+      {openMenu &&
+        <div
+          onClick={() => setOpenMenu(!openMenu)}
+          className="bg-black/60 w-full h-full absolute left-0 top-0 z-10"
+        ></div>
+      }
     </>
   )
 }
